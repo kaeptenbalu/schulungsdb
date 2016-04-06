26f504788a01f282037399049e717ef3bfb84277
z4rd0z <boll06@stud.uni-passau.de>
2016-04-06 05:31:28 +0200

    added session management, some sql bugfixes

--- action.php
+++ action.php
@@ -1,88 +1,187 @@
+<?php session_start(); ?>
 <?php
+/**
+* Login-Stuff
+* To logout -> unset($_SESSION['user_id']);
+**/
+if (isset($_POST["signin"])) {	
+	$username = $_POST["username"];
+	$password = $_POST["password"];
 
-function runSQLquery($query){
-	//connect to db
-	class MyDB extends SQLite3
-	{
-	    function __construct()
-	    {
-		$this->open('./SchulungsDB.db');
-	    }
+	$query = "SELECT username, password FROM User WHERE username = '".$username."' AND password = '".$password."'";
+
+	$results = runSQLquery($query);
+	
+	//echo '<pre>'; print_r($results); echo '</pre>';
+	if ($username === $results[0]['username'] && $results[0]['password']) {
+	    //Sessioncookie
+    	$_SESSION['user'] = $username;
+    		/*array(
+        	'name' => $username,
+        	'login' => 'login',
+        	'some_info' => 'some info'
+    	);*/
+    	echo 'sessioncookie gesetzt. </br> <script type="text/javascript"> window.open("index.html","_self");</script>';
 	}
 
+	else{
+		echo "something went wrong x(";
+	}
+
+}
+
+
+/**
+* Debugging Helper
+**/
+function debug_to_console( $data ) {
+
+    if ( is_array( $data ) )
+        $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+    else
+        $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+
+    echo $output;
+}
+
+
+/**
+* SQL-Helper function
+**/
+
+class MyDB extends SQLite3 {
+    function __construct()
+    {
+    	//connect to db
+		$this->open('./SchulungsDB.db');
+    }
+}
+
+
+function checkResult($result){
+	if (!$result) {
+    	throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
+    	debug_to_console( "db error 2" );
+    	die("SQL-Query Error.");
+	}
+}
+
+function runSQLquery($query, $type){	
+	
 	$db = new MyDB();
-
-	//execute query
-	$result = $db->exec($query);
-	return var_dump($result->fetchArray());
+	debug_to_console( "bla" );
+    if(!$db){
+		echo $db->lastErrorMsg();
+		debug_to_console( "db error" );
+		die("SQL-Query Error.");
+    } 
+    else {
+    	/**
+    	* query-function bug executes each query twice if insert with query is used
+    	* as workarround, each insert call needs to use exec function which not return any datasets
+    	* but true or false.
+    	**/
+    	if ($type === 'insert') {
+	    	debug_to_console( "insert" );
+			$result = $db->exec($query);
+			checkresult($result);
+			return $result;
+		}
+		else {
+			$results = array();
+			$result = $db->query($query);
+			checkresult($result);
+			//debug_to_console( $results );
+			while($row=$result->fetchArray(SQLITE3_ASSOC)){
+	   			$rows = array($row);
+	   			$results = array_merge($results,$rows);
+	   			//debug_to_console( $rows );
+			}
+		}
+	}
+	
+	$db->close();
+	return $results;
 }
 
 
-if (isset($_POST["createTeilnehmer"])) {	
-	$vorname = $_POST["vorname"];
-	$nachname = $_POST["nachname"];
-	$email = $_POST["email"];
-	$firma = $_POST["firma"];
-	$betrieb = $_POST["betrieb"];	
+/**
+* Schulungsfoo
+**/
+if(isset($_SESSION['user'])){
+//logged In
 
-	//prepare query
-	$query = "INSERT INTO Teilnehmer (vorname, nachname, mailadresse, firma, betrieb) VALUES ('".$vorname."', '".$nachname."', '".$email."', '".$firma."', '".$betrieb."');";
+	if (isset($_POST["createTeilnehmer"])) {	
+		$vorname = $_POST["vorname"];
+		$nachname = $_POST["nachname"];
+		$email = $_POST["email"];
+		$firma = $_POST["firma"];
+		$betrieb = $_POST["betrieb"];	
 
-	//execute query
-	$result = runSQLquery($query);
-	//query db
-	//$result = $db->query("SELECT * FROM Teilnehmer;");
-	echo $result;	
+		//prepare query
+		$query = "INSERT INTO Teilnehmer (vorname, nachname, mailadresse, firma, betrieb) VALUES ('".$vorname."', '".$nachname."', '".$email."', '".$firma."', '".$betrieb."');";
+
+		//execute query
+		$result = runSQLquery($query, 'insert');
+		
+		//get List of participants
+		$results = runSQLquery("SELECT * FROM Teilnehmer;", 'select');
+		echo '<pre>'; print_r($results); echo '</pre>';
+		//echo '<pre>'; print_r(var_dump($results)); echo '</pre>';		
+	}
+
+
+	if (isset($_POST["createBetrieb"])){
+		$firma = $_POST["firma"];
+		$strasse = $_POST["strasse"];
+		$plz = $_POST["plz"];
+		$ort = $_POST["ort"];
+		$bildungskoordinator = $_POST["bildungskoordinator"];
+
+		//prepare query
+		$query = "INSERT INTO Betriebe (firma, strasse, plz, ort, bildungskoordinator) VALUES ('".$firma."', '".$strasse."', '".$plz."', '".$ort."', '".$bildungskoordinator."');";
+
+		$result = runSQLquery($query);
+		echo $result;
+	}
+
+
+	if (isset($_POST["createSchulung"])){
+		$name = $_POST["name"];
+		$bezeichnung = $_POST["bezeichnung"];
+		$datum = $_POST["datum"];
+		$schulungsort = $_POST["schulungsort"];
+
+		//prepare query
+		$query = "INSERT INTO Schulungen (name, bezeichnung, datum, schulungsort) VALUES ('".$name."', '".$bezeichnung."', '".$datum."', '".$schulungsort."');";
+
+		$result = runSQLquery($query);
+		//query db
+		//$result = $db->query("SELECT * FROM Teilnehmer;");
+		echo $result;
+	}
+
+
+	if (isset($_POST["createBildungsbeauftragter"])){
+		$name = $_POST["name"];
+		$vorname = $_POST["vorname"];
+		$email = $_POST["email"];
+		$telefonnummer = $_POST["telefonnummer"];
+		
+		//$id = $_POST["id"];
+
+		//prepare query
+		//$query = "INSERT INTO Bildungsbeauftragter (name, vorname, email, telefonnummer, id) VALUES //('".$name."', '".$vorname."', '".$email."', '".$telefonnumer."','".$id."');";
+		$query = "INSERT INTO Bildungsbeauftrager (name, vorname, email, telefonnumer, id) VALUES ('".$name."', '".$vorname."', '".$email."', '".$telefonnummer."', 1);";
+
+		$result = runSQLquery($query);
+		echo $result;
+	}
+
+}else{
+// Not logged in :(
+	echo "you are not logged in. please login...";
 }
 
-
-if (isset($_POST["createBetrieb"])){
-	$firma = $_POST["firma"];
-	$strasse = $_POST["strasse"];
-	$plz = $_POST["plz"];
-	$ort = $_POST["ort"];
-	$bildungskoordinator = $_POST["bildungskoordinator"];
-
-	//prepare query
-	$query = "INSERT INTO Betriebe (firma, strasse, plz, ort, bildungskoordinator) VALUES ('".$firma."', '".$strasse."', '".$plz."', '".$ort."', '".$bildungskoordinator."');";
-
-	$result = runSQLquery($query);
-	//query db
-	//$result = $db->query("SELECT * FROM Teilnehmer;");
-	echo $result;
-}
-
-
-if (isset($_POST["createSchulung"])){
-	$name = $_POST["name"];
-	$bezeichnung = $_POST["bezeichnung"];
-	$datum = $_POST["datum"];
-	$schulungsort = $_POST["schulungsort"];
-
-	//prepare query
-	$query = "INSERT INTO Schulungen (name, bezeichnung, datum, schulungsort) VALUES ('".$name."', '".$bezeichnung."', '".$datum."', '".$schulungsort."');";
-
-	$result = runSQLquery($query);
-	//query db
-	//$result = $db->query("SELECT * FROM Teilnehmer;");
-	echo $result;
-}
-
-
-if (isset($_POST["createBildungsbeauftragter"])){
-	$name = $_POST["name"];
-	$vorname = $_POST["vorname"];
-	$email = $_POST["email"];
-	$telefonnummer = $_POST["telefonnummer"];
-	$id = $_POST["id"];
-
-	//prepare query
-	$query = "INSERT INTO Schulungen (name, vorname, email, telefonnumer, id) VALUES ('".$name."', '".$vorname."', '".$email."', '".$telefonnumer."','".$id."');";
-
-	$result = runSQLquery($query);
-	//query db
-	//$result = $db->query("SELECT * FROM Teilnehmer;");
-	echo $result;
-}
 
 ?>
--- create_DB.php
+++ create_DB.php
@@ -3,31 +3,44 @@
 $db = new SQLite3('./SchulungsDB.db');
 //$dbhandle = sqlite_open('db/test.db', 0666, $error);
 
+//login
+$db->exec('DROP TABLE If EXISTS User');
+$db->query('CREATE TABLE User (ID INTEGER PRIMARY KEY, username varchar(255), password varchar(255));');
+echo "adminaccount wurde eingerichtet </br>";
+
+//create admin
+$query = "INSERT INTO User (username, password) VALUES ('admin', 'secret');";
+$result = $db->exec($query);
+if (!$result) {
+   	throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
+}
+
+
 //tabelle uffrume
 $db->exec('DROP TABLE If EXISTS Teilnehmer');
 $db->exec('DROP TABLE If EXISTS Schulungen');
 $db->exec('DROP TABLE If EXISTS Teilnehmer_Schulungen');
 $db->exec('DROP TABLE If EXISTS Betriebe');
-$db->exec('DROP TABLE If EXISTS Bildungsbeauftrager');
+$db->exec('DROP TABLE If EXISTS Bildungsbeauftragter');
 
 
 //tabelle Teilnehmer erstellen
 $db->query('CREATE TABLE Teilnehmer (ID INTEGER PRIMARY KEY, vorname varchar(255), nachname varchar(255), mailadresse varchar(255), firma varchar(255), betrieb varchar(255))');
-echo "Tabelle 'person' wurde erstellt \n";
+echo "Tabelle 'person' wurde erstellt </br>";
 
 //tabelle Schulungen
 $db->exec('CREATE TABLE Schulungen (ID INTEGER PRIMARY KEY, name varchar(255), bezeichnung varchar(255), datum date, schulungsort varchar(255))');
-echo "Tabelle 'Schulung' wurde erstellt \n";
+echo "Tabelle 'Schulung' wurde erstellt </br>";
 
 //tabelle Teilnehmer_Schulung
 $db->exec('CREATE TABLE Teilnehmer_Schulungen (teilnehmer_id INTEGER NOT NULL, schulung_id INTEGER NOT NULL, FOREIGN KEY(teilnehmer_id) REFERENCES teilnehmer(ID) ON DELETE CASCADE, FOREIGN KEY(schulung_id) REFERENCES Schulungen(ID) ON DELETE CASCADE)');
-echo "Tabelle 'Teilnehmer_Schulung' wurde erstellt \n";
+echo "Tabelle 'Teilnehmer_Schulung' wurde erstellt </br>";
 
 //tabelle Bildungsbeauftragter
-$db->exec('CREATE TABLE Bildungsbeauftrager (ID INTEGER PRIMARY KEY, name varchar(255), vorname varchar(255), email varchar(255), telefonnumer varchar(15))');
-echo "Tabelle 'Bildungsbeauftrager wurde erstellt \n";
+$db->exec('CREATE TABLE Bildungsbeauftragter (ID INTEGER PRIMARY KEY, name varchar(255), vorname varchar(255), email varchar(255), telefonnummer varchar(15))');
+echo "Tabelle 'Bildungsbeauftrager wurde erstellt </br>";
 
 //tabelle Betriebe
 $db->exec('CREATE TABLE Betriebe (betriebsnummer INTEGER PRIMARY KEY, firma varchar(255), strasse varchar(255), plz varchar(10), ort varchar(100), bildungsbeauftrager_id INTEGER NOT NULL, FOREIGN KEY(bildungsbeauftrager_id) REFERENCES Bildungsbeauftrager(ID) ON DELETE CASCADE )');
-echo "Tabelle 'Betriebe' wurde erstellt \n";
+echo "Tabelle 'Betriebe' wurde erstellt </br>";
 ?>
