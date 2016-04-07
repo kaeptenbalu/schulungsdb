e08775096c3c294c282430cd05627b428136c206
z4rd0z <boll06@stud.uni-passau.de>
2016-04-07 18:08:53 +0200

    uffgrummt

--- login.php
+++ login.php
@@ -6,23 +6,22 @@ error_reporting(E_ALL | E_STRICT);
 
 readfile("./template/head.html");
 
-echo '
-        <form action="../action.php" method="POST" class="navbar-form navbar-right" role="form">
-        <input type="hidden" name="signin" value="signin">
-            <div class="input-group">
-                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                <input id="username" type="text" class="form-control" name="username" value="" placeholder="username">                                        
-            </div>
+?>
+<form action="../action.php" method="POST" class="navbar-form navbar-right" role="form">
+    <input type="hidden" name="signin" value="signin">
+    <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+        <input id="username" type="text" class="form-control" name="username" value="" placeholder="username">                                        
+    </div>
 
-            <div class="input-group">
-                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                <input id="password" type="password" class="form-control" name="password" value="" placeholder="Kennwort">                                        
-            </div>
+    <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+        <input id="password" type="password" class="form-control" name="password" value="" placeholder="Kennwort">                                        
+    </div>
 
-            <button type="submit" class="btn btn-primary">Anmelden</button>
-       </form>
-    ';
+    <button type="submit" class="btn btn-primary">Anmelden</button>
+</form>
 
+<?php
 readfile("./template/footer.html");
-
 ?>
